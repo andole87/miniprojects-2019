9e30a4bc2b164d092944d5d33e3fcf16962bda16
andole87 <andole87@gmail.com>
2019-08-26 12:23:00 +0900

    refactor(mediafile): AmazonS3를 빈으로 등록

--- src/main/java/com/woowacourse/zzinbros/common/config/upload/UploadToFactory.java
+++ src/main/java/com/woowacourse/zzinbros/common/config/upload/UploadToFactory.java
@@ -1,5 +1,6 @@
 package com.woowacourse.zzinbros.common.config.upload;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +14,15 @@ public class UploadToFactory {
     @Value("${zzinbros.uploadto.downloadurl}")
     private String downloadUrl;
 
-    public UploadToFactory() {
+    private AmazonS3 amazonS3;
+
+    public UploadToFactory(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
     }
 
     public UploadTo create(MultipartFile file) {
         if (strategy.equals("s3")) {
-            return new UploadToS3(file, uploadUrl, downloadUrl);
+            return new UploadToS3(file, uploadUrl, downloadUrl, amazonS3);
         }
         if (strategy.equals("local")) {
             return new UploadToLocal(file, uploadUrl, downloadUrl);
